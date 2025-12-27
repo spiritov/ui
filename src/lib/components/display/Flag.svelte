790bc3f967b2c6c9cc42e632fb480848ad9e4e42
bailey <spiritov_v@pm.me>
2025-12-27 03:33:00 -0700

    add Flag

---
+++ src/lib/components/display/Flag.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let { code } = $props();
+</script>
+
+<!-- show nothing for no flag -->
+{#if code}
+  <img
+    src="https://flagcdn.com/32x24/{code}.png"
+    srcset="https://flagcdn.com/64x48/{code}.png 2x,
+    https://flagcdn.com/96x72/{code}.png 3x"
+    width="32"
+    height="24"
+    alt=""
+    class="object-none" />
+{/if}
---
+++ src/stories/Flag.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import '../routes/layout.css';
+
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import Flag from '$lib/components/display/Flag.svelte';
+
+  const { Story } = defineMeta({
+    component: Flag
+  });
+</script>
+
+<Story name="Flag" args={{ code: 'us' }}></Story>
