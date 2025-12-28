52a74b76d539cb13fe2e948b5ef7462dd469812e
bailey <spiritov_v@pm.me>
2025-12-27 23:32:41 -0700

    add Player

---
+++ src/lib/components/display/table/Player.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Flag from '../Flag.svelte';
+
+  type Props = {
+    player: Player;
+  };
+
+  let { player }: Props = $props();
+</script>
+
+<div class="flex size-full cursor-pointer items-center gap-1">
+  <Flag code={player.country_code} />
+  <img class="ml-3 size-8 rounded-box" src={player.steam_avatar_url} alt="" />
+  <span class="truncate hover:underline">{player.display_name}</span>
+</div>
