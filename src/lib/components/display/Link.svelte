e94a1611ffb933eeb3cbcf049018da66cca1243d
bailey <spiritov_v@pm.me>
2025-12-27 20:20:06 -0700

    add Link

---
+++ src/lib/components/display/Link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  type Props = {
+    src: string;
+    href: string;
+    label: string;
+  };
+
+  let { src, href, label }: Props = $props();
+</script>
+
+<a class="flex w-fit gap-1 text-primary/75 hover:text-primary hover:underline" {href}>
+  <img class="size-6" {src} alt="" />
+  <span>{label}</span>
+</a>
