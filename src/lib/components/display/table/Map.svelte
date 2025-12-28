4cd9d46f6710d94a0a4af7084e409d179251372b
bailey <spiritov_v@pm.me>
2025-12-27 23:32:33 -0700

    add Map

---
+++ src/lib/components/display/table/Map.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  type Props = {
+    map: string;
+  };
+
+  let { map }: Props = $props();
+</script>
+
+<div
+  class="grid size-full place-content-center mask-x-from-90% bg-cover bg-center"
+  style:background-image={`url("https://tempusplaza.xyz/map-backgrounds/${map}.jpg")`}>
+  <span class="truncate text-shadow-base-900 text-shadow-sm">{map}</span>
+</div>
