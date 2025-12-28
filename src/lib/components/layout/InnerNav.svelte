4f70504d3789c25dd5c9963a83e60ebda09bc97b
bailey <spiritov_v@pm.me>
2025-12-27 19:12:45 -0700

    add InnerNav

---
+++ src/lib/components/layout/InnerNav.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { page } from '$app/state';
+
+  type Props = {
+    route: string;
+    parentRoute: string;
+    pages: string[];
+  };
+
+  let { route, parentRoute, pages }: Props = $props();
+</script>
+
+<nav class="flex h-12 w-full text-lg transition-colors select-none hover:text-content/75">
+  <div class="relative flex items-center">
+    {#each pages as label}
+      {@render page(label)}
+    {/each}
+    <hr class="absolute bottom-2.5 left-1/24 w-11/12 rounded-box text-base-700" />
+  </div>
+</nav>
+
+{#snippet page(label: string)}
+  {@const currentRoute: boolean = (route.includes(label))}
+  <a
+    class="relative px-2 hover:text-content {currentRoute ? 'text-content' : ''}"
+    href="/{parentRoute}/{label}">
+    <span>{label} route</span>
+    {#if currentRoute}
+      <hr class="absolute bottom-0 left-1/12 z-10 w-5/6 rounded-box border border-primary" />
+    {/if}
+  </a>
+{/snippet}
