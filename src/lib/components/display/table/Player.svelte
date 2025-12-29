c26d343e75e75beb0b6fe66f30f5d739881f9f5d
bailey <spiritov_v@pm.me>
2025-12-29 04:38:15 -0700

    update Table Player

--- src/lib/components/display/table/Player.svelte
+++ src/lib/components/display/table/Player.svelte
@@ -9,8 +9,8 @@
 </script>
 
 <div
-  class="group flex size-full cursor-pointer items-center gap-2 rounded-box px-2 hover:border hover:border-content/50 hover:bg-base-900">
+  class="group flex size-full cursor-pointer items-center gap-1 rounded-box px-2 hover:border hover:border-content/50 hover:bg-base-900">
   <Flag code={player.country_code} />
-  <img class="ml-4 size-8 rounded-box" src={player.steam_avatar_url} alt="" />
+  <img class="ml-2 size-8 rounded-box" src={player.steam_avatar_url} alt="" />
   <span class="truncate group-hover:underline">{player.display_name}</span>
 </div>
