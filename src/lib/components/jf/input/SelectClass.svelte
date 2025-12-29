9abf5d9387538d440fa8e27bad30ffea453632d4
bailey <spiritov_v@pm.me>
2025-12-28 22:27:06 -0700

    update SelectClass

--- src/lib/components/jf/input/SelectClass.svelte
+++ src/lib/components/jf/input/SelectClass.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
   import demo from '$lib/assets/tf/demo.png';
   import soldier from '$lib/assets/tf/soldier.png';
+
+  type Props = {
+    selected: string;
+    onsubmit: (value: string) => void;
+  };
+
+  let { selected, onsubmit }: Props = $props();
 </script>
 
-<div class="flex">
-  <img class="object-none" src={soldier} alt="" />
-  <img class="object-none" src={demo} alt="" />
-</div>
+<!-- bring your own container -->
+
+<!-- soldier -->
+<button
+  class="cursor-pointer p-2 transition-opacity
+  {selected === 'Demo' ? 'opacity-25 hover:opacity-75' : ''}"
+  onclick={() => {
+    console.log('clicked');
+    onsubmit('Soldier');
+    selected = 'Soldier';
+  }}>
+  <img class="max-w-16 object-cover" src={soldier} alt="" />
+</button>
+<!-- demo -->
+<button
+  class="cursor-pointer p-2 transition-opacity
+  {selected === 'Soldier' ? 'opacity-25 hover:opacity-75' : ''}"
+  onclick={() => {
+    onsubmit('Demo');
+    selected = 'Demo';
+  }}>
+  <img class="max-w-16 object-cover" src={demo} alt="" />
+</button>
 
 <style>
   /* match theme content color */
