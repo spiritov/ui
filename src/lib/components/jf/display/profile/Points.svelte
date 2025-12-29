397a62819f9ecb3a686640db9a966e382a48e6b3
bailey <spiritov_v@pm.me>
2025-12-29 04:45:53 -0700

    add Points

---
+++ src/lib/components/jf/display/profile/Points.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  type Props = {
+    points: ClassPoints;
+  };
+
+  let { points }: Props = $props();
+</script>
+
+<div class="flex gap-2">
+  {@render category('last 3 monthly', points.last_3_monthly)}
+  {@render category('last 9 motw', points.last_9_motw)}
+  {@render category('total', points.total)}
+</div>
+
+{#snippet category(label: string, value: number)}
+  <div class="flex h-fit min-w-24 flex-col bg-base-700/0">
+    <span class="text-sm text-content/75">{label}</span>
+    <span>{value}</span>
+  </div>
+{/snippet}
